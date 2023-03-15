ed9f1da36e531dc1b6b0a79b71cdba240a0bc6e6
AliElsheikh <47045107+AliEl4eikh@users.noreply.github.com>
2023-03-15 15:29:57 +0200

    add vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,15 @@
-import '@/styles/globals.css'
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
+import "@/styles/globals.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { Analytics } from "@vercel/analytics/react";
+
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
